23a10ce9cf959d621a72275871a3e3061138dd69
keith12brown <keith12brown@msn.com>
2019-05-14 08:38:21 -0600

    Modified how usage of the mark.

--- tic-tac-toe-client/src/app/tic-tac-toe-tile/tic-tac-toe-tile.component.ts
+++ tic-tac-toe-client/src/app/tic-tac-toe-tile/tic-tac-toe-tile.component.ts
@@ -26,7 +26,7 @@ export class TicTacToeTileComponent implements AfterViewInit {
 
   @Input() tile: Tile;
 
-  @Input() mark: Mark;
+  //@Input() mark: Mark;
 
   @Output() clicked: EventEmitter<{ row: number, col: number }> = new EventEmitter();
 
