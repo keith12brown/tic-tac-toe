69efd880b6f49e396d01d1e27756d98ddfab58a0
Keith12Brown <keith12brown@msn.com>
2020-12-12 15:00:30 -0700

    Changeds due to upgrading.
    Angular CLI: 11.0.4
    
    Angular: 11.0.4
    
    typescript    4.0.5

--- tic-tac-toe-client/src/app/tic-tac-toe-board/tic-tac-toe-board.component.spec.ts
+++ tic-tac-toe-client/src/app/tic-tac-toe-board/tic-tac-toe-board.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TicTacToeBoardComponent } from './tic-tac-toe-board.component';
 
@@ -6,7 +6,7 @@ describe('TicTacToeBoardComponent', () => {
   let component: TicTacToeBoardComponent;
   let fixture: ComponentFixture<TicTacToeBoardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TicTacToeBoardComponent ]
     })
--- tic-tac-toe-client/src/app/tic-tac-toe-tile/tic-tac-toe-tile.component.spec.ts
+++ tic-tac-toe-client/src/app/tic-tac-toe-tile/tic-tac-toe-tile.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TicTacToeTileComponent } from './tic-tac-toe-tile.component';
 
@@ -6,7 +6,7 @@ describe('TicTacToeTileComponent', () => {
   let component: TicTacToeTileComponent;
   let fixture: ComponentFixture<TicTacToeTileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TicTacToeTileComponent ]
     })
