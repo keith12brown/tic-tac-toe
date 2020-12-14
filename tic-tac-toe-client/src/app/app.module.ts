a4c4d7a40e5e55f0dfd0529a9964eab3d9748204
Keith12Brown <keith12brown@msn.com>
2020-12-14 13:03:18 -0700

    Fixed materials not being a module.

--- tic-tac-toe-client/src/app/app.module.ts
+++ tic-tac-toe-client/src/app/app.module.ts
@@ -2,104 +2,97 @@ import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatIconRegistry,
-  MatIconModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
 import { AppComponent } from './app.component';
 import { TicTacToeBoardComponent } from './tic-tac-toe-board/tic-tac-toe-board.component';
 import { TicTacToeTileComponent } from './tic-tac-toe-tile/tic-tac-toe-tile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TicTacToeBoardComponent,
-    TicTacToeTileComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatInputModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatTooltipModule
-  ],
-  exports: [
-    BrowserAnimationsModule,
-    MatAutocompleteModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatStepperModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TicTacToeBoardComponent,
+        TicTacToeTileComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatInputModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatTooltipModule
+    ],
+    exports: [
+        BrowserAnimationsModule,
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- tic-tac-toe-client/src/app/tic-tac-toe-board/tic-tac-toe-board.component.ts
+++ tic-tac-toe-client/src/app/tic-tac-toe-board/tic-tac-toe-board.component.ts
@@ -1,269 +1,267 @@
 import {
-  Component,
-  OnInit,
-  Input,
-  Output
+    Component,
+    OnInit,
+    Input,
+    Output
 } from '@angular/core';
 import { WebSocketService } from '../websocket.service';
 import {
-  Move,
-  Player,
-  TicTacToeMessage,
-  Mark
+    Move,
+    Player,
+    TicTacToeMessage,
+    Mark
 } from 'projects/tic-tac-toe-lib/src/lib/tic-tac-toe-message';
 import { TicTacToeTileComponent, Tile } from '../tic-tac-toe-tile/tic-tac-toe-tile.component';
-import {
-  MatIconRegistry,
-  MatTooltip,
-} from '@angular/material';
+import { MatIconRegistry } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BoardEvaluateService } from '../board-evaluate.service';
 import { FormControl } from '@angular/forms';
 import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-tic-tac-toe-board',
-  templateUrl: './tic-tac-toe-board.component.html',
-  styleUrls: ['./tic-tac-toe-board.component.css']
+    selector: 'app-tic-tac-toe-board',
+    templateUrl: './tic-tac-toe-board.component.html',
+    styleUrls: ['./tic-tac-toe-board.component.css']
 })
 export class TicTacToeBoardComponent implements OnInit {
 
-  player: Player;
+    player: Player;
 
-  private tiles: Tile[] = new Array<Tile>();
+    private tiles: Tile[] = new Array<Tile>();
 
-  showDelay = new FormControl(500);
-  hideDelay = new FormControl(1000);
+    showDelay = new FormControl(500);
+    hideDelay = new FormControl(1000);
 
-  private get opponent(): Player | undefined {
-    return this.player ? this.player.opponent : undefined;
-  }
-
-  public get mark(): Mark {
-    return this.player ? this.player.mark : '';
-  }
-
-  public get opponentMark(): Mark {
-    return this.player && this.opponent ? this.opponent.mark : '';
-  }
-
-  public get thisPlayerGoStop(): string {
-    return !this.canPlay() ? (this.enabled ? 'player-go' : 'player-stop') : 'player-stop';
-  }
-
-  public get oppPlayerGoStop(): string {
-    return !this.canPlay() ? (!this.enabled ? 'player-go' : 'player-stop') : 'player-stop';
-  }
-
-  private playerName = '';
-
-  private _connectionColor: string;
-  public get connectionColor(): string {
-    if (!this.connected && this.error) {
-      this._connectionColor = 'warn';
-    } else if (!this.connected) {
-      this._connectionColor = 'disabled';
-    } else if (this.connected) {
-      this._connectionColor = 'primary';
-    }
-    return this._connectionColor;
-  }
-
-  public get cursorClass(): string {
-    let returnValue = '';
-    if ((!this.enabled && !this.mark) || this.winner) {
-      returnValue = 'not-allowed';
-    } else if (!this.enabled && this.mark) {
-      returnValue = 'wait';
-    } else {
-      returnValue = '';
-    }
-    return returnValue;
-  }
-
-  private connected = false;
-
-  private error = false;
-
-  private replay = false;
-
-  private _enabled = false;
-  public get enabled() {
-    return this._enabled;
-  }
-  public set enabled(value) {
-    this._enabled = value;
-  }
-
-  private winner: Player = undefined;
-
-  constructor(
-    private evaluationService: BoardEvaluateService,
-    private socket: WebSocketService,
-    iconRegistry: MatIconRegistry,
-    sanitizer: DomSanitizer
-  ) {
-    iconRegistry.addSvgIcon(
-      'cloud',
-      sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/graphics/baseline-cloud-24px.svg'
-      )
-    );
-    iconRegistry.addSvgIcon(
-      'cloud-off',
-      sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/graphics/baseline-cloud_off-24px.svg'
-      )
-    );
-    iconRegistry.addSvgIcon(
-      'X',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/graphics/x.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'O',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/graphics/o.svg')
-    );
-    iconRegistry.addSvgIcon(
-      'hourglass-full',
-      sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/graphics/baseline-hourglass_full-24px.svg'
-      )
-    );
-
-    let index = 0;
-    for (let row = 0; row < 3; ++row) {
-      for (let col = 0; col < 3; ++col) {
-        const t = new Tile(row, col);
-        this.tiles[index++] = t;
-      }
-    }
-  }
-
-  ngOnInit() {
-    this.socket.opponentConnected$.subscribe(opp => {
-      this.player.opponent = opp;
-      this.player.mark = opp.mark === 'X' ? 'O' : 'X';
-      this.enabled = this.player.mark === 'X';
-    });
-
-    this.socket.error$.subscribe(error => error ? this.error = true : false);
-
-    this.socket.message$.subscribe(message => console.log(message));
-
-    this.socket.connected$.subscribe(value => {
-      this.connected = value;
-      this.player = { mark: undefined, name: this.playerName, quit: false };
-      this.socket.registerPlayer(this.player);
-      this.replay = false;
-    });
-
-    this.socket.opponentMove$.subscribe(oppMove => {
-      this.setTile(oppMove);
-      this.detectWinner(oppMove);
-      this.enabled = !this.replay;
-    });
-
-    this.socket.opponentQuit$.subscribe(oppQuit => {
-      this.opponentQuit();
-    });
-  }
-
-  playerTooltip(): string {
-    let result = '';
-    if (this.player) {
-      if (this.opponent.quit) {
-        result = `You win. ${this.opponent.name} quit`;
-      } else if (this.winner) {
-        result = this.winner === this.player ? 'You win' : `${this.opponent.name} wins`;
-      } else if (this.tiles.every(t => t.mark ? true : false)) {
-        result = 'Tie';
-      } else {
-        result = this.enabled ? 'Your turn' : `Waiting on ${this.opponent.name}`;
-      }
-    }
-    return result;
-  }
-
-  canPlay() {
-    return this.playerName && (!this.connected || this.replay);
-  }
-
-  clickPlay() {
-    if (!this.connected) {
-      this.socket.connect();
-    } else {
-      this.socket.registerPlayer(this.player);
-    }
-    this.replay = false;
-    this.clearBoard();
-  }
-
-  onKeyUp(event: KeyboardEvent) {
-    const target = <HTMLInputElement>event.target;
-    this.playerName = target.value;
-    if (event.key === 'Enter' && this.canPlay()) {
-      this.clickPlay();
-    }
-  }
-
-  tileClick(move: Move) {
-    const tile = this.findTile(move.row, move.col);
-    if (this.enabled && !tile.mark) {
-      tile.mark = this.mark;
-      move.mark = this.mark;
-      const message: TicTacToeMessage = new TicTacToeMessage(move, this.playerName);
-      this.socket.send(message);
-      this.detectWinner(move);
-      this.enabled = false;
-    }
-  }
-
-  private opponentQuit(): void {
-    if (this.winner) {
-      return;
+    private get opponent(): Player | undefined {
+        return this.player ? this.player.opponent : undefined;
     }
 
-    this.tiles.forEach(t => {
-      if (t.mark === this.opponentMark) {
-        t.mark = '?';
-      }
-    });
-
-    this.player.opponent.quit = true;
-    this.enabled = false;
-    this.replay = true;
-  }
-
-  private findTile(row: number, col: number): Tile {
-    return this.tiles[row * 3 + col];
-  }
-
-  private clearBoard(): void {
-    this.tiles.forEach((tile) => tile.clear());
-    this.winner = undefined;
-    if (this.player) {
-      this.player.opponent = undefined;
-    }
-    this.enabled = false;
-  }
-
-  private setTile(move: Move): void {
-    const tile = this.findTile(move.row, move.col);
-    if (tile) {
-      tile.mark = <Mark>move.mark;
-    }
-  }
-
-  private detectWinner(move: Move): void {
-    const result = this.evaluationService.evaluate(move);
-
-    if (result) {
-      this.winner = result.mark === this.mark ? this.player : this.opponent;
-      result.tiles.forEach(tile => this.tiles[tile.row * 3 + tile.col].isWinner$.next(true));
+    public get mark(): Mark {
+        return this.player ? this.player.mark : '';
     }
 
-    if (result || this.tiles.every(t => t.mark ? true : false)) {
-      this.replay = true;
+    public get opponentMark(): Mark {
+        return this.player && this.opponent ? this.opponent.mark : '';
     }
-  }
+
+    public get thisPlayerGoStop(): string {
+        return !this.canPlay() ? (this.enabled ? 'player-go' : 'player-stop') : 'player-stop';
+    }
+
+    public get oppPlayerGoStop(): string {
+        return !this.canPlay() ? (!this.enabled ? 'player-go' : 'player-stop') : 'player-stop';
+    }
+
+    private playerName = '';
+
+    private _connectionColor: string;
+    public get connectionColor(): string {
+        if (!this.connected && this.error) {
+            this._connectionColor = 'warn';
+        } else if (!this.connected) {
+            this._connectionColor = 'disabled';
+        } else if (this.connected) {
+            this._connectionColor = 'primary';
+        }
+        return this._connectionColor;
+    }
+
+    public get cursorClass(): string {
+        let returnValue = '';
+        if ((!this.enabled && !this.mark) || this.winner) {
+            returnValue = 'not-allowed';
+        } else if (!this.enabled && this.mark) {
+            returnValue = 'wait';
+        } else {
+            returnValue = '';
+        }
+        return returnValue;
+    }
+
+    private connected = false;
+
+    private error = false;
+
+    private replay = false;
+
+    private _enabled = false;
+    public get enabled() {
+        return this._enabled;
+    }
+    public set enabled(value) {
+        this._enabled = value;
+    }
+
+    private winner: Player = undefined;
+
+    constructor(
+        private evaluationService: BoardEvaluateService,
+        private socket: WebSocketService,
+        iconRegistry: MatIconRegistry,
+        sanitizer: DomSanitizer
+    ) {
+        iconRegistry.addSvgIcon(
+            'cloud',
+            sanitizer.bypassSecurityTrustResourceUrl(
+                'assets/graphics/baseline-cloud-24px.svg'
+            )
+        );
+        iconRegistry.addSvgIcon(
+            'cloud-off',
+            sanitizer.bypassSecurityTrustResourceUrl(
+                'assets/graphics/baseline-cloud_off-24px.svg'
+            )
+        );
+        iconRegistry.addSvgIcon(
+            'X',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/graphics/x.svg')
+        );
+        iconRegistry.addSvgIcon(
+            'O',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/graphics/o.svg')
+        );
+        iconRegistry.addSvgIcon(
+            'hourglass-full',
+            sanitizer.bypassSecurityTrustResourceUrl(
+                'assets/graphics/baseline-hourglass_full-24px.svg'
+            )
+        );
+
+        let index = 0;
+        for (let row = 0; row < 3; ++row) {
+            for (let col = 0; col < 3; ++col) {
+                const t = new Tile(row, col);
+                this.tiles[index++] = t;
+            }
+        }
+    }
+
+    ngOnInit() {
+        this.socket.opponentConnected$.subscribe(opp => {
+            this.player.opponent = opp;
+            this.player.mark = opp.mark === 'X' ? 'O' : 'X';
+            this.enabled = this.player.mark === 'X';
+        });
+
+        this.socket.error$.subscribe(error => error ? this.error = true : false);
+
+        this.socket.message$.subscribe(message => console.log(message));
+
+        this.socket.connected$.subscribe(value => {
+            this.connected = value;
+            this.player = { mark: undefined, name: this.playerName, quit: false };
+            this.socket.registerPlayer(this.player);
+            this.replay = false;
+        });
+
+        this.socket.opponentMove$.subscribe(oppMove => {
+            this.setTile(oppMove);
+            this.detectWinner(oppMove);
+            this.enabled = !this.replay;
+        });
+
+        this.socket.opponentQuit$.subscribe(oppQuit => {
+            this.opponentQuit();
+        });
+    }
+
+    playerTooltip(): string {
+        let result = '';
+        if (this.player) {
+            if (this.opponent.quit) {
+                result = `You win. ${this.opponent.name} quit`;
+            } else if (this.winner) {
+                result = this.winner === this.player ? 'You win' : `${this.opponent.name} wins`;
+            } else if (this.tiles.every(t => t.mark ? true : false)) {
+                result = 'Tie';
+            } else {
+                result = this.enabled ? 'Your turn' : `Waiting on ${this.opponent.name}`;
+            }
+        }
+        return result;
+    }
+
+    canPlay() {
+        return this.playerName && (!this.connected || this.replay);
+    }
+
+    clickPlay() {
+        if (!this.connected) {
+            this.socket.connect();
+        } else {
+            this.socket.registerPlayer(this.player);
+        }
+        this.replay = false;
+        this.clearBoard();
+    }
+
+    onKeyUp(event: KeyboardEvent) {
+        const target = <HTMLInputElement>event.target;
+        this.playerName = target.value;
+        if (event.key === 'Enter' && this.canPlay()) {
+            this.clickPlay();
+        }
+    }
+
+    tileClick(move: Move) {
+        const tile = this.findTile(move.row, move.col);
+        if (this.enabled && !tile.mark) {
+            tile.mark = this.mark;
+            move.mark = this.mark;
+            const message: TicTacToeMessage = new TicTacToeMessage(move, this.playerName);
+            this.socket.send(message);
+            this.detectWinner(move);
+            this.enabled = false;
+        }
+    }
+
+    private opponentQuit(): void {
+        if (this.winner) {
+            return;
+        }
+
+        this.tiles.forEach(t => {
+            if (t.mark === this.opponentMark) {
+                t.mark = '?';
+            }
+        });
+
+        this.player.opponent.quit = true;
+        this.enabled = false;
+        this.replay = true;
+    }
+
+    private findTile(row: number, col: number): Tile {
+        return this.tiles[row * 3 + col];
+    }
+
+    private clearBoard(): void {
+        this.tiles.forEach((tile) => tile.clear());
+        this.winner = undefined;
+        if (this.player) {
+            this.player.opponent = undefined;
+        }
+        this.enabled = false;
+    }
+
+    private setTile(move: Move): void {
+        const tile = this.findTile(move.row, move.col);
+        if (tile) {
+            tile.mark = <Mark>move.mark;
+        }
+    }
+
+    private detectWinner(move: Move): void {
+        const result = this.evaluationService.evaluate(move);
+
+        if (result) {
+            this.winner = result.mark === this.mark ? this.player : this.opponent;
+            result.tiles.forEach(tile => this.tiles[tile.row * 3 + tile.col].isWinner$.next(true));
+        }
+
+        if (result || this.tiles.every(t => t.mark ? true : false)) {
+            this.replay = true;
+        }
+    }
 }
