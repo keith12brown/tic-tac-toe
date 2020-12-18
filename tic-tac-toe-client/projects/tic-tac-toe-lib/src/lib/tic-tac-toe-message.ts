7d76cb7bd81acadaa26dbb233c4df3e32057849b
Keith12Brown <keith12brown@msn.com>
2020-12-18 16:46:15 -0700

    Added kind to all message interfaces.
    Added message type.
    
    Added create functions.

--- tic-tac-toe-client/projects/tic-tac-toe-lib/src/lib/tic-tac-toe-message.ts
+++ tic-tac-toe-client/projects/tic-tac-toe-lib/src/lib/tic-tac-toe-message.ts
@@ -1,23 +1,67 @@
+
 export interface Move {
+    kind: 'move';
     row: number;
     col: number;
     mark: Mark;
 }
 
 export interface Player {
+    kind: 'player';
     name: string;
-    opponent?: Player;
-    mark: Mark;
     quit: boolean;
+    opponent?: Player;
+    mark?: Mark;
 }
 
 export interface ConnectionStatus {
+    kind: 'connectionStatus';
     success: boolean;
     message?: string;
 }
 
+export interface Information {
+    kind: 'info';
+    info: string;
+}
+
 export class TicTacToeMessage {
-    constructor(public content: Move | Player | ConnectionStatus  | string, public sender: string) { }
+    constructor(public content: Move | Player | ConnectionStatus | Information, public sender: string) { }
 }
 
 export type Mark = 'X' | 'O' | '?' | '';
+
+export type MessageType = Move | Player | ConnectionStatus | Information;
+
+
+export function getMessageContent(message: MessageType): Move | Player | ConnectionStatus | Information {
+    switch (message.kind) {
+        case 'player':
+            return message;
+        case 'move':
+            return message;
+        case 'connectionStatus':
+            return message;
+        case 'info':
+            return message;
+    }
+}
+
+export function createMove(row: number, col: number, mark: Mark): Move {
+    return { kind: 'move', row: row, col: col, mark: mark };
+}
+
+export function createPlayer(name: string,
+    quit: boolean,
+    opponent?: Player,
+    mark?: Mark): Player {
+    return { kind: 'player', name: name, quit: quit, opponent: opponent, mark: mark };
+}
+
+export function createConnectionStatus(success: boolean, message?: string): ConnectionStatus {
+    return { kind: 'connectionStatus', success: success, message: message };
+}
+
+export function createInformation(info: string): Information {
+    return { kind: 'info', info: info };
+}
