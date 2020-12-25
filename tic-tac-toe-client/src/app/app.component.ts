cd14ed074d5dc2057bfe84ea0085d8b069375b6a
Keith12Brown <keith12brown@msn.com>
2020-12-25 08:15:28 -0700

    Removed unused imports.

--- tic-tac-toe-client/src/app/app.component.ts
+++ tic-tac-toe-client/src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { Component, HostListener } from '@angular/core';
-import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
-import { TicTacToeMessage as Message } from '../../projects/tic-tac-toe-lib/src/lib/tic-tac-toe-message';
-import { Subject } from 'rxjs';
-import { WebSocketService } from './websocket.service';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
