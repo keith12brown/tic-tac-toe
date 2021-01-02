c7eff487cd0ba7d77028ddaeca4e802de3bd4451
Keith12Brown <keith12brown@msn.com>
2021-01-02 11:39:59 -0700

    Removed unnecessary import.

--- tic-tac-toe-client/src/app/app.module.ts
+++ tic-tac-toe-client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
